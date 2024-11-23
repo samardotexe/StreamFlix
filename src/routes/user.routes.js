e1c4578ef265812d89b24a8a8328d11d34260cee
samar-ali-ansari <sanardotexe@gmail.com>
2024-11-23 18:30:35 +0530

    Created routes for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,23 @@
 import { Router } from "express"
 
-import { registerUser, logoutUser, loginUser, refreshAccessToken } from "../controllers/userController.js"
+import { registerUser,
+    loginUser,
+    refreshAccessToken,
+    logoutUser,
+    changeCurrentPassword,
+    getCurrentUser, updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory } from "../controllers/userController.js"
 
 import {uploads} from "../middlewears/multer.middlewear.js"
 
 import { verifyJWT } from "../middlewears/authmiddlewears.js"
 
-
+//initiating router
 const router = Router()
-//when someone hits /api/v1/healthcheck/test
+
 
 router.route("/register").post(
     upload.fields([
@@ -28,10 +37,15 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 router.route("/refresh-token").post(refreshAccessToken)
 
-
-
 //secured routes
-router.route("logout").post(verifyJWT,logoutUser)
+router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get( verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 
 export default router
